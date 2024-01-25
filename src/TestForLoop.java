043d36561713a9243759352bcf95bf750b9618f1
Ashwinikondam <157654747+Ashwinikondam@users.noreply.github.com>
2024-01-24 19:10:12 -0600

    Modified Forloop to 10 times

--- src/TestForLoop.java
+++ src/TestForLoop.java
@@ -6,7 +6,7 @@ public class TestForLoop {
 
     public static void main(String[] args) {
 
-        for(int i=0; i<5; i++) {
+        for(int i=0; i<10; i++) {
             System.out.println("***Printing in Forloop.");
         }
     }
